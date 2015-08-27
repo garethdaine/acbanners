cb889585749a795add7b19528d9a38f5b0702cde
Gareth Daine <garethdaine@googlemail.com>
2015-08-27 09:01:18 +0100

    PSR2 changes.

--- app/code/community/Acbanners/Block/Adminhtml/Acbannerbuttons.php
+++ app/code/community/Acbanners/Block/Adminhtml/Acbannerbuttons.php
@@ -1,13 +1,13 @@
 <?php
  
-class AffinityCloud_Acbanners_Block_Adminhtml_Acbannerbuttons extends Mage_Adminhtml_Block_Widget_Grid_Container
-{
+class AffinityCloud_Acbanners_Block_Adminhtml_Acbannerbuttons extends Mage_Adminhtml_Block_Widget_Grid_Container {
     public function __construct()
     {
         $this->_controller = 'adminhtml_acbannerbuttons';
         $this->_blockGroup = 'acbanners';
         $this->_headerText = Mage::helper('acbanners')->__('Manage Banner Buttons');
         $this->_addButtonLabel = Mage::helper('acbanners')->__('Add Banner Button');
+
         parent::__construct();
     }
 }
